d7ee8d73c1f04fd2d9924287283646ff0e706743
rekasa <regeecasana57@gmail.com>
2025-06-06 22:28:29 +0800

    Initialize: hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc.d
@@ -0,0 +1,5 @@
+/home/rekasa/Projects/syspace/hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc: src/main.rs
+
+/home/rekasa/Projects/syspace/hello_cargo/target/debug/deps/hello_cargo-2e9734ea5f186fdc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/rekasa/Projects/syspace/hello_cargo/target/debug/hello_cargo: /home/rekasa/Projects/syspace/hello_cargo/src/main.rs
